6fc83516ad9bea87151877003c778a08b93cedc1
Safinxr <safinxr@gmail.com>
2022-03-26 00:50:13 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import PurchaseArea from './component/PurchaseArea/PurchaseArea';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='text-center fw-bolder text-danger font-size'>Anime Aelector</h1>
+      <PurchaseArea></PurchaseArea>
     </div>
   );
 }
---
+++ src/component/PurchaseArea/PurchaseArea.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './PurchaseArea.css'
+
+const PurchaseArea = () => {
+    return (
+        <div>
+                
+        </div>
+    );
+};
+
+export default PurchaseArea;
